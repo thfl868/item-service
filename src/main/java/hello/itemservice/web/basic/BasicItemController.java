36685b8cca866405d92e670139b8e6dcb76ecaea
imsori <dlathfl868@naver.com>
2022-12-16 17:07:44 +0900

    RedirectAttributes 사용

--- src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.annotation.PostConstruct;
 import java.util.List;
@@ -81,14 +82,22 @@ public class BasicItemController {
         return "basic/item";
     }
 
-    @PostMapping("/add")
-    public String addItemV5( Item item){
+    //@PostMapping("/add")
+    public String addItemV5(Item item){
 
         itemRepository.save(item);
-        //model.addAttribute("item", item);
-        //@ModelAttribute가 item객체를 가져오고, 모델에 자동으로 추가해줌(생략가능)
 
         return "redirect:/basic/items/"+item.getId();
+    }
+
+    @PostMapping("/add")
+    public String addItemV6(Item item, RedirectAttributes redirectAttributes){
+
+        Item savedItem = itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId", savedItem.getId());
+        redirectAttributes.addAttribute("status", true);
+
+        return "redirect:/basic/items/{itemId}";
     }
 
     @GetMapping("/{itemId}/edit")
